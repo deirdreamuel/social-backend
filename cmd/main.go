edcd334cd6feeabdf5bad4b6c786989c15a43a2a
Deirdre Chong <xerzen@live.com>
2023-01-06 00:35:25 -0600

    fix: edit cors config to proper values

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"speakeasy/internal/app"
 	"speakeasy/internal/pkg/authentication"
 	"speakeasy/internal/pkg/trip"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -15,7 +16,14 @@ func main() {
 	godotenv.Load()
 
 	router := gin.Default()
-	router.Use(cors.Default())
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000", "https://amuel.org", "https://dev.amuel.org"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization", "User-Agent"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	// Create services
 	authenticationSvc := authentication.NewAuthenticationService()
